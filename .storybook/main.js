21c134a71010ccd73ad6bc852d63f6746a87885a
zhangrenyang <zhang_renyang@126.com>
2020-08-20 13:36:29 +0800

    init

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,6 +14,15 @@ module.exports = {
       use: [
         {
           loader: require.resolve("react-docgen-typescript-loader"),
+          options: {
+            shouldExtractLiteralValuesFromEnum: true,
+            propFilter: (prop) => {
+              if (prop.parent) {
+                return !prop.parent.fileName.includes("node_modules");
+              }
+              return true;
+            },
+          },
         },
       ],
     });
